fa771280fc1fcfc59d680171ebc175a6ebef30ae
Lovinder Pnag <lspnag@gmail.com>
2016-02-25 16:11:28 -0500

    can not start before 17

---
+++ lib/babysitter.rb
@@ -0,0 +1,7 @@
+class BabySit
+  def initialize start, endtime
+    @start, @endtime = start, endtime
+  end
+
+  
+end
--- spec/babysitter_spec.rb
+++ spec/babysitter_spec.rb
@@ -1,8 +1,16 @@
 describe 'BabySit' do
   let(:hour) {double('hour')}
-  subject {Babysit.new(hour, hour)}
+  subject {BabySit.new(hour, hour)}
 
   it 'expect 2 arguments' do
     expect(subject.method(:initialize).arity).to eq 2
+  end
+
+  describe '#valid_schedule' do
+
+    it 'invalid schedule earlier than 5pm' do
+      subject.instance_variable_set(:@start, 16)
+      expect(subject.valid_schedule).to eq false
+    end
   end
 end
